5d84fdc4ccecd52d1ea7a1671d754efe267a6623
jiangfan <1565133958@qq.com>
2022-07-13 17:14:31 +0800

    账单系统初始化

---
+++ bill/src/main/java/com/jiangfan/bill/controller/BilldetailController.java
@@ -0,0 +1,12 @@
+package com.jiangfan.bill.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ：jiangfan
+ * @date ：2022/7/13 17:13
+ * @description：
+ */
+@RestController
+public class BilldetailController {
+}
